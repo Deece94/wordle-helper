33239f0c2835277a30e11c76871d33ee4a5743fc
Deece94 <=>
2023-10-29 20:25:35 +1100

    changed effect to memo function. Changed semi correct guesses to be 2d array. Fixed up css file naming

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, ChangeEventHandler, useEffect, useState } from 'react';
+import { ChangeEvent, useMemo, useState } from 'react';
 import dictionary from './dictionary';
 import './App.css';
 
@@ -10,9 +10,12 @@ function App() {
     '',
     '',
   ]);
-  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+  const initialArray: string[][] = Array(5)
+    .fill([])
+    .map(() => []);
+  const [guessedLetters, setGuessedLetters] =
+    useState<string[][]>(initialArray); // a 2d array of guessed letters
   const [incorrectLetters, setIncorrectLetters] = useState<string[]>([]);
-  const [guesses, setGuesses] = useState<string[]>([]);
   const [shownGuesses, setShownGuesses] = useState<number>(5);
 
   const updateKnown = (e: ChangeEvent) => {
@@ -39,71 +42,26 @@ function App() {
     }
   };
 
+  // Updates the guessed letters array for a specific letter position
   const updateGuessedLetters = (e: ChangeEvent<HTMLTextAreaElement>) => {
     const input = e.target as HTMLTextAreaElement;
     const newGuessedLetters = input.value.toLowerCase().split('');
-    setGuessedLetters(newGuessedLetters);
+    const newGuessedLettersArray = [...guessedLetters];
+    const pos = Number(input.dataset.pos);
+
+    newGuessedLettersArray[pos] = newGuessedLetters;
+    setGuessedLetters(newGuessedLettersArray);
   };
 
+  // Updates the incorrect letters array
   const updateIncorrectLetters = (e: ChangeEvent<HTMLTextAreaElement>) => {
     const input = e.target as HTMLTextAreaElement;
     const newIncorrectLetters = input.value.toLowerCase().split('');
     setIncorrectLetters(newIncorrectLetters);
   };
 
-  useEffect(() => {
-    const newGuesses: string[] = [];
-
-    const shuffledDictionary: string[] = sortArray(dictionary);
-
-    shuffledDictionary.every((guessWord) => {
-      const lowerCaseWord = guessWord.toLowerCase();
-
-      // Check known letters are in the correct spot in the word
-      for (let i = 0; i < 5; i++) {
-        if (
-          lowerCaseWord.charAt(i) !== knownLetters[i] &&
-          knownLetters[i] !== ''
-        ) {
-          return true; // Continue
-        }
-      }
-
-      // Check all semi correct letters are in the word
-      let containsGuessedLetters = true;
-      guessedLetters.every((letter) => {
-        if (!lowerCaseWord.includes(letter)) {
-          containsGuessedLetters = false;
-          return false; // Break
-        }
-        return true; // Continue
-      });
-      if (!containsGuessedLetters) {
-        return true; // Continue
-      }
-
-      // Check all incorrect letters are in the word
-      let containsIncorrectLetters = false;
-      incorrectLetters.every((letter) => {
-        if (lowerCaseWord.includes(letter)) {
-          containsIncorrectLetters = true;
-          return false; // Break
-        }
-        return true; // Continue
-      });
-      if (containsIncorrectLetters) {
-        return true; // Continue
-      }
-
-      newGuesses.push(lowerCaseWord);
-      return true; // Continue
-    });
-
-    setGuesses(newGuesses);
-    setShownGuesses(5);
-  }, [knownLetters, guessedLetters, incorrectLetters]);
-
-  const sortArray = (array: string[]) => {
+  // Shuffles array in place
+  const shuffleArray = (array: string[]) => {
     let currentIndex = array.length,
       randomIndex;
 
@@ -123,13 +81,74 @@ function App() {
     return array;
   };
 
+  const guesses = useMemo(() => {
+    const newGuesses: string[] = [];
+
+    const shuffledDictionary: string[] = shuffleArray(dictionary);
+
+    shuffledDictionary.forEach((guessWord) => {
+      const lowerCaseWord = guessWord.toLowerCase();
+
+      // Check known letters are in the correct spot in the word
+      for (let i = 0; i < 5; i++) {
+        if (
+          lowerCaseWord.charAt(i) !== knownLetters[i] &&
+          knownLetters[i] !== ''
+        ) {
+          return; // Continue to next word
+        }
+      }
+
+      // Check semi correct letters array to see if they are in the word but not at this position
+      const fulfillsGuessedLetters = guessedLetters.every((letters, index) => {
+        // If no letters have been guessed for this position, continue
+        if (letters.length === 0) {
+          return true;
+        }
+
+        // Check each guessed letter to see if it is in the word in another position but not this one
+        const fulfillsGuessedLetter = letters.every((letter) => {
+          if (
+            lowerCaseWord.includes(letter) &&
+            lowerCaseWord.charAt(index) !== letter
+          ) {
+            return true;
+          }
+          return false;
+        });
+
+        return fulfillsGuessedLetter;
+      });
+      if (!fulfillsGuessedLetters) {
+        return;
+      }
+
+      // Check all incorrect letters are in the word
+      let containsIncorrectLetters = incorrectLetters.some((letter) => {
+        if (lowerCaseWord.includes(letter)) {
+          return true;
+        }
+        return false;
+      });
+      if (containsIncorrectLetters) {
+        return;
+      }
+
+      newGuesses.push(lowerCaseWord);
+    });
+
+    setShownGuesses(5);
+
+    return newGuesses;
+  }, [knownLetters, guessedLetters, incorrectLetters]);
+
   const nf = new Intl.NumberFormat();
 
   return (
     <>
-      <h1 className="headerTitle">Wordle Helper</h1>
+      <h1 className="header-title">Wordle Helper</h1>
       <div className="container">
-        <div className="knownLetters">
+        <div className="known-section">
           <input className="letter" data-pos={0} onChange={updateKnown} />
           <input className="letter" data-pos={1} onChange={updateKnown} />
           <input className="letter" data-pos={2} onChange={updateKnown} />
@@ -137,38 +156,59 @@ function App() {
           <input className="letter" data-pos={4} onChange={updateKnown} />
         </div>
 
-        <div className="guessesSection">
-          <h2 className="title">Semi correct Letters</h2>
+        <h2 className="title">Semi correct Letters</h2>
+        <div className="guesses-section">
           <textarea
-            className="guesses"
+            className="guessed-letter"
             onChange={updateGuessedLetters}
+            data-pos={0}
+          ></textarea>
+          <textarea
+            className="guessed-letter"
+            onChange={updateGuessedLetters}
+            data-pos={1}
+          ></textarea>
+          <textarea
+            className="guessed-letter"
+            onChange={updateGuessedLetters}
+            data-pos={2}
+          ></textarea>
+          <textarea
+            className="guessed-letter"
+            onChange={updateGuessedLetters}
+            data-pos={3}
+          ></textarea>
+          <textarea
+            className="guessed-letter"
+            onChange={updateGuessedLetters}
+            data-pos={4}
           ></textarea>
         </div>
 
-        <div className="guessesSection">
-          <h2 className="title">Incorrect Letters</h2>
+        <h2 className="title">Incorrect Letters</h2>
+        <div className="incorrect-section">
           <textarea
             className="guesses"
             onChange={updateIncorrectLetters}
           ></textarea>
         </div>
 
-        <div className="suggestionsSection">
-          <h2 className="title">Suggestions</h2>
-          <ul className="guessList">
+        <h2 className="title">Suggestions</h2>
+        <div className="suggestions-section">
+          <ul className="guess-list">
             {guesses.slice(0, shownGuesses).map((guess, index) => {
               return (
-                <li className="listItem" key={index}>
+                <li className="guess-list-item" key={index}>
                   {guess}
                 </li>
               );
             })}
             {guesses.length > shownGuesses && (
               <>
-                <li className="listItem">
+                <li className="guess-list-item">
                   + {nf.format(guesses.length - shownGuesses)} more
                 </li>
-                <li className="listItem">
+                <li className="guess-list-item">
                   <button
                     onClick={() => {
                       setShownGuesses(shownGuesses + 5);
